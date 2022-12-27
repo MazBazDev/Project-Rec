8d7906975b548e0a209e425c2a1bc811bbaa3106
MazBaz <mrlog42@gmail.com>
2022-12-27 22:43:02 +0100

    Remove vendor

--- resources/views/layouts/elements/admin/footer.blade.php
+++ resources/views/layouts/elements/admin/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>{{ setting('site.name', env("APP_NAME")) }} &copy; | 2021 - {{ date("Y") }}</span>
+            <span>{{ setting('settings.site.name', env("APP_NAME")) }} &copy; | 2021 - {{ date("Y") }}</span>
         </div>
     </div>
 </footer>
